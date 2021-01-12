8bacbaa2d1c17f3361a5124931b9634656945fa9
Aleks <nikolic.aleks90@gmail.com>
2021-01-12 13:02:27 -0500

    ez ternary implementation nothing went wrong

--- quickqueue/src/components/StoreFront.tsx
+++ quickqueue/src/components/StoreFront.tsx
@@ -1,13 +1,21 @@
-import React, { SyntheticEvent, useState } from 'react';
-import { useHistory } from "react-router-dom";
+import React, { SyntheticEvent, useState, useContext } from 'react';
+import { useHistory, Redirect } from "react-router-dom";
 import axios from 'axios';
+import { UserContext } from '../App';
 
 export const StoreFront: React.FunctionComponent<any> = (props) => {
 
+  let currentUser = useContext(UserContext);
+  console.log(currentUser)
+
   return (
 
-    <h1>TEMP TESTING IF REDIRECT WORKS</h1>
-  
-)
+      (currentUser) ?
+      <h1>MAIN STORE PAGE</h1>
+      :
+      <Redirect to='/login'/>
+
+
+  )
 
 }
