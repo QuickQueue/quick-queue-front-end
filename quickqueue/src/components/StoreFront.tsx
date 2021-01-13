64f2dca2906b6e00d591e14b489055a818bc456c
kelvintrinh174 <kelvintrinh174@gmail.com>
2021-01-13 14:56:34 -0500

    update Menu Drawer

--- quickqueue/src/components/StoreFront.tsx
+++ quickqueue/src/components/StoreFront.tsx
@@ -3,10 +3,18 @@ import { useHistory, Redirect } from "react-router-dom";
 import axios from "axios";
 import { UserContext } from "../App";
 import { NavBar } from "./NavBar";
+import { ProductDetail } from "./ProductDetail";
 
 export const StoreFront: React.FunctionComponent<any> = (props) => {
   let currentUser = useContext(UserContext);
   console.log(currentUser);
 
-  return currentUser ? <NavBar /> : <Redirect to="/login" />;
+  return currentUser ? (
+    <>
+      <NavBar />
+      <ProductDetail />
+    </>
+  ) : (
+    <Redirect to="/login" />
+  );
 };
