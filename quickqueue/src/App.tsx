3a4facdb0fe457da8c6a9eb88ace05cd5e9d9a1e
Raviraj <ravirajdodiya2@yahoo.com>
2021-01-13 10:57:22 -0500

    updated

--- quickqueue/src/App.tsx
+++ quickqueue/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import { LoginForm } from './components/LoginForm'
+import { LoginForm}  from './components/loginForm';
+import { SearchItem } from './components/SearchItem';
 
 function App() {
   return (
     <div className="App">
       <LoginForm/>
+      <SearchItem/>
     </div>
   );
 }
