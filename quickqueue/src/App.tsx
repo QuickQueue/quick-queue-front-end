be2fad85e56929b37aa3f2bd26401a21a5a59366
Colin Shaw <colin.shaw2@uoit.net>
2021-01-13 23:45:53 -0500

    items are retrieved from api and store in state

--- quickqueue/src/App.tsx
+++ quickqueue/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { LoginForm } from "./components/LoginForm";
+import { ItemView } from "./components/ItemView";
 import { StoreFront } from "./components/StoreFront";
 import { RegisterForm } from "./components/RegisterForm";
 import { User } from "./models/Users";
@@ -28,6 +29,11 @@ function App() {
             <Route path="/register">
               <RegisterForm isCustomer={true} />
             </Route>
+
+            <Route path="/items">
+              <ItemView />
+            </Route>
+
             <Route path="/" render={() => <h1>No path was chosen</h1>} />
           </Switch>
         </Router>
