ef7db36a1b9806bd56f077891c73c2a83395372c
Aleks <nikolic.aleks90@gmail.com>
2021-01-16 12:12:49 -0500

    added a new profile navbar component

--- quickqueue/src/components/Profile.tsx
+++ quickqueue/src/components/Profile.tsx
@@ -1,15 +1,25 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../App';
 import { useHistory, Redirect } from "react-router-dom";
+import { ProfileNavBar } from './ProfileNavBar';
 
 export const Profile: React.FunctionComponent<any> = () => {
 
   let currentUser = useContext(UserContext)
 
+  //add nav bar
+  // create onclick links for log out and store and redirect
+  //style nav bar
+  //create subheading for history
+  //dropdown for active orders first
+
 
   return currentUser ? (
+    <>
+    <ProfileNavBar/>
 
     <h1>THIS YOUR PROFILE HOMIEEE</h1>
+    </>
 
   ) : (
 
